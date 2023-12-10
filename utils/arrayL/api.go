b3113738575f88c302951707aa0929a08cb46649
flyerxp <570937581@qq.com>
2023-12-10 11:25:52 +0800

    不同类型数组去重

--- utils/arrayL/api.go
+++ utils/arrayL/api.go
@@ -1,13 +1,59 @@
 package arrayL
 
+import "strconv"
+
 // 去重
 func UniqArr(nums []int) []int {
 	unique := make(map[int]bool)
-	result := []int{}
-	for _, num := range nums {
-		if !unique[num] {
-			result = append(result, num)
-			unique[num] = true
+	var result []int
+	for i := range nums {
+		if !unique[nums[i]] {
+			result = append(result, nums[i])
+			unique[nums[i]] = true
+		}
+	}
+	return result
+}
+func UniqArr32(nums []int32) []int32 {
+	unique := make(map[int32]bool)
+	var result []int32
+	for i := range nums {
+		if !unique[nums[i]] {
+			result = append(result, nums[i])
+			unique[nums[i]] = true
+		}
+	}
+	return result
+}
+func UniqArr32ToInt(nums []int32) []int {
+	unique := make(map[int32]bool)
+	var result []int
+	for i := range nums {
+		if !unique[nums[i]] {
+			result = append(result, int(nums[i]))
+			unique[nums[i]] = true
+		}
+	}
+	return result
+}
+func UniqArr32ToString(nums []int32) []string {
+	unique := make(map[int32]bool)
+	var result []string
+	for i := range nums {
+		if !unique[nums[i]] {
+			result = append(result, strconv.Itoa(int(nums[i])))
+			unique[nums[i]] = true
+		}
+	}
+	return result
+}
+func UniqArrToString(nums []int) []string {
+	unique := make(map[int]bool)
+	var result []string
+	for i := range nums {
+		if !unique[nums[i]] {
+			result = append(result, strconv.Itoa(nums[i]))
+			unique[nums[i]] = true
 		}
 	}
 	return result
---
+++ utils/arrayL/test/arr_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"fmt"
+	"github.com/flyerxp/lib/utils/arrayL"
+	"testing"
+)
+
+func TestArrConv(t *testing.T) {
+	ids32 := []int32{1, 2, 3, 1, 2, 3}
+	ids := []int{1, 2, 3, 1, 2, 3}
+	fmt.Printf("%#v\n", arrayL.UniqArr(ids))
+	fmt.Printf("%#v\n", arrayL.UniqArr32(ids32))
+	fmt.Printf("%#v\n", arrayL.UniqArr32ToInt(ids32))
+	fmt.Printf("%#v\n", arrayL.UniqArrToString(ids))
+	fmt.Printf("%#v\n", arrayL.UniqArr32ToInt(ids32))
+	fmt.Printf("%#v\n", arrayL.UniqArr32ToString(ids32))
+}
